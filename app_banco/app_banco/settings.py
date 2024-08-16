3015f135c7c4bcffb05664d95d74f8de273c1336
Julian Imac <julian.maya@gmail.com>
2024-08-15 19:41:07 -0500

    final deploy

--- app_banco/app_banco/settings.py
+++ app_banco/app_banco/settings.py
@@ -80,10 +80,26 @@ WSGI_APPLICATION = 'app_banco.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+# DATABASES = {
+#     'default': {
+#         "ENGINE": "django.db.backends.mysql",
+#         'NAME': 'banco',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        'NAME': 'JulianMayaUniand$banco',
+        'USER': 'JulianMayaUniand',
+        'PASSWORD': 'hjkl7890',
+        'HOST': 'JulianMayaUniandes.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
